6f639d38d947a71195bb1cf5c7b98be9eda0518e
Neko Of The Abyss <psi@innocent.com>
2022-08-24 18:35:53 +0530

    comment

--- use/math/useRegression.ts
+++ use/math/useRegression.ts
@@ -5,6 +5,12 @@ interface LinearRegression {
     r2: number;
 }
 
+/**
+ * A linear regression using the Ordinary Least Squares method.
+ * @param x An array of independent values.
+ * @param y An array of dependent values. 
+ * @returns An object with slope, intercept, r2 score, and a prediction function.
+ */
 export function useLinearRegression(x: number[], y: number[]): LinearRegression {
     const n =  Math.min(x.length, y.length);
     const mean = [0, 0];
@@ -42,6 +48,4 @@ export function useLinearRegression(x: number[], y: number[]): LinearRegression 
         predict,
         r2: ssr/sst
     }
-
-
 }
