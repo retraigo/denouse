6c037d34cf93abf950a1a9e28d86f4a89d5340b1
NeTT <nett@nett.moe>
2023-07-26 17:42:07 +0530

    fix type again

--- use/array/split.ts
+++ use/array/split.ts
@@ -1,7 +1,7 @@
 import { useShuffle } from "../random/shuffle.ts";
 
-export interface Sliceable {
-  slice<T>(start?: number | undefined, end?: number | undefined): Sliceable & T;
+export interface Sliceable<T> extends ArrayLike<T> {
+  slice(start?: number | undefined, end?: number | undefined): Sliceable<T>;
 }
 
 interface SplitOptions {
@@ -12,7 +12,7 @@ interface SplitOptions {
 /** Split arrays by their first axis */
 export function useSplit(
   options: SplitOptions = { ratio: [7, 3], shuffle: false },
-  ...arr: (ArrayLike<unknown> & Sliceable)[]
+  ...arr: (Sliceable<unknown>)[]
 ): [typeof arr, typeof arr] {
   if (!arr.every((x) => x.length === arr[0].length)) {
     throw new Error("All arrays must have equal length!");
@@ -20,18 +20,17 @@ export function useSplit(
   const { ratio, shuffle } = options;
   const idx = Math.floor(arr[0].length * (ratio[0] / (ratio[0] + ratio[1])));
   if (!shuffle) {
-    return [arr.map((x) => x.slice<typeof x>(0, idx)), arr.map((x) => x.slice<typeof x>(idx))];
+    return [
+      arr.map((x) => x.slice(0, idx)),
+      arr.map((x) => x.slice(idx)),
+    ];
   } else {
     const shuffled = useShuffle(0, arr[0].length);
     const x1 = shuffled.slice(0, idx);
     const x2 = shuffled.slice(idx);
     return [
-      arr.map((x) =>
-        x1.map((i) => x[i])
-      ) as typeof arr,
-      arr.map((x) =>
-        x2.map((i) => x[i])
-      ) as typeof arr,
+      arr.map((x) => x1.map((i) => x[i])) as typeof arr,
+      arr.map((x) => x2.map((i) => x[i])) as typeof arr,
     ];
   }
 }
