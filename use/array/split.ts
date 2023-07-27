f0bc156292805bf25ac62f50a143e77350b61734
NeTT <nett@nett.moe>
2023-07-27 23:52:44 +0530

    update for matrix

--- use/array/split.ts
+++ use/array/split.ts
@@ -1,14 +1,19 @@
 import { useShuffle } from "../random/shuffle.ts";
 
-export interface Sliceable<T> extends ArrayLike<T> {
-  slice(start?: number | undefined, end?: number | undefined): Sliceable<T>;
+export interface Sliceable<T> {
+  filter<S extends T>(
+    predicate: (value: T, index: number) => boolean,
+  ): Sliceable<T>;
+  slice(start?: number, end?: number): Sliceable<T>;
+  length: number;
+  at(pos: number): Sliceable<T>;
 }
 
 interface SplitOptions {
   ratio: [number, number];
   shuffle?: boolean;
 }
-
+Array.prototype;
 /** Split arrays by their first axis */
 export function useSplit(
   options: SplitOptions = { ratio: [7, 3], shuffle: false },
@@ -29,8 +34,8 @@ export function useSplit(
     const x1 = shuffled.slice(0, idx);
     const x2 = shuffled.slice(idx);
     return [
-      arr.map((x) => x1.map((i) => x[i])) as typeof arr,
-      arr.map((x) => x2.map((i) => x[i])) as typeof arr,
+      arr.map((x) => x.filter((_, i) => x1.includes(i))) as typeof arr,
+      arr.map((x) => x.filter((_, i) => x2.includes(i))) as typeof arr,
     ];
   }
 }
